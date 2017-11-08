6fb3fca3a77505cf03e8b5a3784cf4dd8e2a93ff
ygchina <ygchina@hotmail.com>
2017-11-08 20:30:38 +0800

    change folder name

---
+++ heisenberg/routes/SKURoute.js
@@ -0,0 +1,22 @@
+
+const DB = require('../models/db');
+const VersionRepo = require('../services/versionRepo');
+const router = require('koa-router')();
+
+router.prefix("/api/version");
+
+router.get('/all', async function (ctx, next) {
+  ctx.compress = true;
+  var vr = await new VersionRepo().getAll();
+  ctx.body = vr;
+  
+});
+
+
+router.get('/last', async function (ctx, next) {
+  ctx.compress = true;
+  var vr = await new VersionRepo().getNew(); 
+  ctx.body = vr;
+})
+
+module.exports = router;
