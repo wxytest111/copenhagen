87e2769841f0e237a91a049d1f4796d9aa93bc91
yxiuw <ahhbyxw@126.com>
2017-12-12 15:03:42 +0800

    add time field

--- Heisenberg/models/promotion.js
+++ Heisenberg/models/promotion.js
@@ -1,26 +1,31 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('promotion', {
-      id: {
-        type: DataTypes.INTEGER(11).UNSIGNED,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      name: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-        defaultValue: ''
-      },
-      desc: {
-        type: DataTypes.STRING(1000),
-        allowNull: true,
-        defaultValue: ''
-      }
+  return sequelize.define('promotion', {
+    id: {
+      type: DataTypes.INTEGER(11).UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    desc: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
     
-    }, {
-      tableName: 'promotion',    timestamps: false,  
-    });
-  };
-  
+  }, {
+    tableName: 'promotion',    timestamps: false, 
+  });
+};
