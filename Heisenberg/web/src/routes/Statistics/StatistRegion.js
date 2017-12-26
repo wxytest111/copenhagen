d7f05c27cb7946d795de1a9c1f1c3b05b65b9e4c
隋英杰 <714084927@qq.com>
2017-12-26 18:58:30 +0800

    增加热力图

--- Heisenberg/web/src/routes/Statistics/StatistRegion.js
+++ Heisenberg/web/src/routes/Statistics/StatistRegion.js
@@ -2,12 +2,16 @@ import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import { Tabs, Card, Row, Col, Button, Icon, Radio, Menu, Table } from 'antd';
 import ReactEcharts from 'echarts-for-react';
+import echarts from 'echarts';
+import bmap from 'echarts/extension/bmap/bmap';
 import moment from 'moment';
 //引入地图文件
 import 'echarts/map/js/china.js';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import { getTimeDistance } from '../../utils/utils';
 import styles from './StatistRegion.less';
+import tracksData from "../../assets/data/tracks.json";
+import buslines from "../../assets/data/bus_lines.json";
 
 // @Form.create()
 @connect(state => ({
@@ -25,13 +29,243 @@ export default class StatistRegion extends PureComponent {
   //   console.log(key, type);
   //   this.setState({ [type]: key });
   // }
-  
+  constructor(props) {
+    super(props);
+  }
   componentDidMount() {
+    
     let data = [];
     this.getData(data);
-    // 初始化echarts实例
-    // let myChartLine = echarts.init(document.getElementById('main'));
-    // 绘制图表
+    // this.initalECharts(buslines);
+    this.initalEChartshot(tracksData)
+  }
+  initalEChartshot(data) {
+    const myChart = echarts.init(document.getElementById('map'));
+    var points = [].concat.apply([], data.map(function (track) {
+      return track.map(function (seg) {
+        return seg.coord.concat([1]);
+      });
+    }));
+    myChart.setOption({
+      animation: false,
+      bmap: {
+        center: [116.46, 39.92],
+        zoom: 4,
+        roam: true
+      },
+      visualMap: {
+        show: false,
+        top: 'top',
+        min: 0,
+        max: 30,
+        seriesIndex: 0,
+        calculable: true,
+        inRange: {
+          color: ['blue', 'green', 'yellow', 'red']
+        }
+      },
+      series: [{
+        type: 'heatmap',
+        coordinateSystem: 'bmap',
+        data: points,
+        pointSize: 5,
+        blurSize: 6
+      }]
+    });
+  }
+  
+  initalECharts(data) {
+    const myChart = echarts.init(document.getElementById('map'));
+    const hStep = 300 / (data.length - 1);
+    const busLines = [].concat.apply([], data.map(function (busLine, idx) {
+      let prevPt;
+      let points = [];
+      for (let i = 0; i < busLine.length; i += 2) {
+        let pt = [busLine[i], busLine[i + 1]];
+        if (i > 0) {
+          pt = [
+            prevPt[0] + pt[0],
+            prevPt[1] + pt[1]
+          ];
+        }
+        prevPt = pt;
+
+        points.push([pt[0] / 1e4, pt[1] / 1e4]);
+      }
+      return {
+        coords: points,
+        lineStyle: {
+          normal: {
+            color: echarts.color.modifyHSL('#5A94DF', Math.round(hStep * idx))
+          }
+        }
+      };
+    }));
+
+    myChart.setOption({
+      bmap: {
+        center: [116.46, 39.92],
+        zoom: 10,
+        roam: true,
+        mapStyle: {
+          'styleJson': [
+            {
+              'featureType': 'water',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#031628'
+              }
+            },
+            {
+              'featureType': 'land',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#000102'
+              }
+            },
+            {
+              'featureType': 'highway',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            },
+            {
+              'featureType': 'arterial',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'arterial',
+              'elementType': 'geometry.stroke',
+              'stylers': {
+                'color': '#0b3d51'
+              }
+            },
+            {
+              'featureType': 'local',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'railway',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'railway',
+              'elementType': 'geometry.stroke',
+              'stylers': {
+                'color': '#08304b'
+              }
+            },
+            {
+              'featureType': 'subway',
+              'elementType': 'geometry',
+              'stylers': {
+                'lightness': -70
+              }
+            },
+            {
+              'featureType': 'building',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'all',
+              'elementType': 'labels.text.fill',
+              'stylers': {
+                'color': '#857f7f'
+              }
+            },
+            {
+              'featureType': 'all',
+              'elementType': 'labels.text.stroke',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'building',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#022338'
+              }
+            },
+            {
+              'featureType': 'green',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#062032'
+              }
+            },
+            {
+              'featureType': 'boundary',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#465b6c'
+              }
+            },
+            {
+              'featureType': 'manmade',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#022338'
+              }
+            },
+            {
+              'featureType': 'label',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }
+          ]
+        }
+      },
+      series: [{
+        type: 'lines',
+        coordinateSystem: 'bmap',
+        polyline: true,
+        data: busLines,
+        silent: true,
+        lineStyle: {
+          normal: {
+            // color: '#c23531',
+            // color: 'rgb(200, 35, 45)',
+            opacity: 0.2,
+            width: 1
+          }
+        },
+        progressiveThreshold: 500,
+        progressive: 200
+      }, {
+        type: 'lines',
+        coordinateSystem: 'bmap',
+        polyline: true,
+        data: busLines,
+        lineStyle: {
+          normal: {
+            width: 0
+          }
+        },
+        effect: {
+          constantSpeed: 20,
+          show: true,
+          trailLength: 0.1,
+          symbolSize: 1.5
+        },
+        zlevel: 1
+      }]
+    });
   }
 
   getOtionMap() {
@@ -209,7 +443,6 @@ export default class StatistRegion extends PureComponent {
   selectDate = (type) => {
     this.setState({
       rangePickerValue: type,
-      dis:type=='sex'?undefined:'none',
     });
   }
 
@@ -319,30 +552,23 @@ export default class StatistRegion extends PureComponent {
         key: 'companyName' + i + 1,
       });
     }
-    const mapHtml = this.state.rangePickerValue == 'plane' ?
-      <ReactEcharts
-        ref={(e) => {
-          this.echarts_react = e;
-        }}
-        option={this.getOtionMap()}
-        notMerge={true}
-        lazyUpdate={true}
-        // style={{ height: '300px', width: '100%' }}
-        className='react_for_echarts' /> : 
-      <ReactEcharts
-        ref={(e) => {
-          this.echarts_react = e;
-        }}
-        option={this.getOtionMap()}
-        notMerge={true}
-        lazyUpdate={true}
-        // style={{ height: '300px', width: '100%' }}
-        className='react_for_echarts' />;
+    // const mapHtml = this.state.rangePickerValue == 'plane' ?
+    //   <ReactEcharts
+    //     ref={(e) => {
+    //       this.echarts_react = e;
+    //     }}
+    //     option={this.getOtionMap()}
+    //     notMerge={true}
+    //     lazyUpdate={true}
+    //     // style={{ height: '300px', width: '100%' }}
+    //     className='react_for_echarts' /> : 
+    //   <div id="map" style={{ width: '100%', height: 300, }} />;
     
     
 
 
     return (
+      
       <PageHeaderLayout>
         <Menu
           theme="light"
@@ -360,7 +586,7 @@ export default class StatistRegion extends PureComponent {
         <Row gutter={16} style={{ marginBottom: 16 }}>
           <Col span={24}>
             <Card style={{ width: '100%'}} bordered={false} bodyStyle= {{padding:10}} >
-              <Row type="flex" justify="space-between" align="bottom" style={{display:this.state.dis?undefined:'none'}}>
+              <Row type="flex" justify="space-between" align="bottom" >
                 <Col span={12}>
                   <Row>
                     <div className={styles.salesExtra}>
@@ -373,8 +599,21 @@ export default class StatistRegion extends PureComponent {
                     </div>
                   </Row>
                   <div style={{ paddingLeft: 30, paddingRight: 15 }}>
-                    {mapHtml}
-                  </div> 
+                    {/* {mapHtml} */}
+                    <div className={this.state.rangePickerValue == 'plane' ? styles.mapshow : styles.maphide}>
+                      <ReactEcharts
+                        ref={(e) => {
+                          this.echarts_react = e;
+                        }}
+                        option={this.getOtionMap()}
+                        notMerge={true}
+                        lazyUpdate={true}
+                        className='react_for_echarts' /> 
+                    </div>
+                    <div className={this.state.rangePickerValue == 'plane' ? styles.maphide : styles.mapHotshow}>
+                      <div id="map" style={{width:'100%', height:300}}></div>
+                    </div> 
+                  </div>
                 
                 </Col>
                 <Col span={12}>
