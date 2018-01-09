823d1d2841b6a0e8838f66cb57077a3407535660
xinyu <30540047@qq.com>
2018-01-09 10:16:13 +0800

    add Empty deploy job

---
+++ Heisenberg/scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+ssh root@106.75.81.187 /bin/bash << EOF
+cd /root/copenhagen
+pwd
+#git pr
+#npm install
+#forever restart
+EOF
--- Heisenberg/scripts/prepare.sh
+++ Heisenberg/scripts/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.bash_profile
+
 set -e
 
 nvm list
