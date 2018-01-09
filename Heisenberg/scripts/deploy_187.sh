e219b0ce8025520c0c3e00a4f4e09e55cdcd30d0
xinyu <30540047@qq.com>
2018-01-09 17:47:57 +0800

    cp dist to public in ci

---
+++ Heisenberg/scripts/deploy_187.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+ssh root@106.75.81.187 /bin/bash << EOF
+cd /root/copenhagen
+pwd
+git co .
+git pr
+cd Heisenberg
+cp -R web/dist/* public/
+forever stop bin/www
+forever start bin/www
+EOF
