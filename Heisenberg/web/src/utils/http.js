4aff48fe60086bebff0346af8144fe25b3732dfc
xinyu <30540047@qq.com>
2018-01-09 14:13:54 +0800

    use ENV to set front end enviroment

--- Heisenberg/web/src/utils/http.js
+++ Heisenberg/web/src/utils/http.js
@@ -1,22 +1,22 @@
-const status = 'develop';
+// const status = 'location';
 const address = {
-    location: "127.0.0.1:3000",
-    develop: "console.tman.ai",
-    production: "47.92.107.250",
+    development: "127.0.0.1:3000",
+    production: "console.tman.ai",
+    staging: "47.92.107.250",
     test:"192.168.1.175:3000"
 };
-let data = '';
-if (status == 'location') {
-    data = address.location;
-} else if (status == 'develop') {
-    data = address.develop;
-} else if (status == 'production'){
-    data = address.production;
-} else {
-    data = address.test;
-}
+// let data = '';
+// if (status == 'location') {
+//     data = address.location;
+// } else if (status == 'develop') {
+//     data = address.develop;
+// } else if (status == 'production'){
+//     data = address.production;
+// } else {
+//     data = address.test;
+// }
 export function gethttpaddress() {
-    return data;
+    return address[process.env.NODE_ENV||'development'];
 }
-
-export default data;
+//
+// export default data;
